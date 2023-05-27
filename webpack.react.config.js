56cc186f0ce4c20d7dd7f89b4a33b48b157c42ca
TheApplePieGod <evant.002@gmail.com>
2023-05-27 14:49:39 -0500

    Build fix

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -1,45 +1,44 @@
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 var config = {
-    entry: './src/frontend/app.tsx',
-    target: 'electron-renderer',
-    devtool: 'source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    }, 
-    module: {
-      rules: [
+  entry: "./src/frontend/app.tsx",
+  target: "electron-renderer",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
       {
         test: /\.ts(x?)$/,
-        include: /src\/frontend/,
-        loader: "ts-loader"
+        exclude: /node_modules/,
+        loader: "ts-loader",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-    },
-    devServer: {
-      contentBase: path.join(__dirname, './dist'),
-      historyApiFallback: true,
-      compress: true,
-      hot: true,
-      port: 4000,
-      publicPath: '/',
-    },
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'bundle.js'
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './html/index.html'
-      })
+        use: ["style-loader", "css-loader"],
+      },
     ],
-  };
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./dist"),
+    historyApiFallback: true,
+    compress: true,
+    hot: true,
+    port: 4000,
+    publicPath: "/",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./html/index.html",
+    }),
+  ],
+};
 
 //config.target = webpackTargetElectronRenderer(config);
 
