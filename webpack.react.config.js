408d12d961072af4062ebc8a5eceeaf63286a12f
TheApplePieGod <evant.002@gmail.com>
2024-12-15 13:19:12 -0600

    Update deps

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -1,45 +1,47 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-var config = {
-  entry: "./src/frontend/app.tsx",
-  target: "electron-renderer",
-  devtool: "source-map",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        loader: "ts-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "./dist"),
-    historyApiFallback: true,
-    compress: true,
-    hot: true,
-    port: 4000,
-    publicPath: "/",
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./html/index.html",
-    }),
-  ],
-};
+module.exports = (env) => {
+    const dev = env.dev;
 
-//config.target = webpackTargetElectronRenderer(config);
+    var config = {
+      entry: "./src/frontend/app.tsx",
+      target: "electron-renderer",
+      devtool: dev ? "source-map" : undefined,
+      resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            loader: "ts-loader",
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
+      devServer: {
+        compress: true,
+        hot: true,
+        port: 4000,
+        static: {
+            directory: path.join(__dirname, "src"),
+        }
+      },
+      output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "./html/index.html",
+        }),
+      ],
+    };
 
-module.exports = config;
+    return config;
+}
